3d4908565a20270cf44bed7ce8d6c996d04d049d
Jorgen Baertsch <jorgenpaulmusic@gmail.com>
2023-01-30 22:16:49 -0800

    Finished save button and time block population

--- Develop/script.js
+++ Develop/script.js
@@ -1,88 +1,70 @@
 //Create all time slots in html
-//use QuerySelectorAll to run a for loop through each table section
+//use $ to select and run a for loop through each table section
 // Use an if loop to determine wether the current hour is the same, less than or more than the hour indicated on the time block
 // Use jquery to change the color of the background of each table element
 
-var timeBlocks = $('.time-block');
-var saveButton = $('.saveBtn');
 
-// saveButton.on('click',function(){
-//   console.log(this);
-//   //need to save text in text area associated with the button clicked to local storage
-//   var task = $(this).prev().value;
+//wraps all code in a jquery call so code is ran after html is loaded
+$(function () {
+  var timeBlocks = $('.time-block');
+  var saveButton = $('.saveBtn');
+ 
+  //Updates color of each timeblock depending on hour provided by day.js
+  function setBlocks(){
+    var hour = dayjs().hour();
+    console.log(hour);
+    //loop through time blocks and compare hour with the id hour displayed.
+    //Set appropriate class according to hour 
+    for(var i = 0; i < timeBlocks.length; i++){
+      //Extract Id
+      
+      var id = $(timeBlocks[i]).attr('id');
+      
+      //Pull number from id string
+      var idTime = (id.match(/(\d+)/));
+      idTime = idTime[0];
+      console.log(idTime);
   
-//   console.log(task);
-//   var storageName = $(this).parent().attr('id');
-//   localStorage.setItem(storageName, task); 
-// })
-
-function setBlocks(){
-  var hour = dayjs().hour();
-  console.log(hour);
-  //loop through time blocks and compare hour with the id hour displayed.
-  //Set appropriate class according to hour 
-  for(var i = 0; i < timeBlocks.length; i++){
-    //Extract Id
-    
-    var id = $(timeBlocks[i]).attr('id');
-    
-    //Pull number from id string
-    var idTime = (id.match(/(\d+)/));
-    idTime = idTime[0];
-    console.log(idTime);
-
-    //create loop comparing idTime and hour
-    if (idTime < hour){
-      $(timeBlocks[i]).removeClass('present');
-      $(timeBlocks[i]).removeClass('future');
-      $(timeBlocks[i]).addClass('past');
-    }
-    else if (idTime == hour){
-      $(timeBlocks[i]).removeClass('future');
-      $(timeBlocks[i]).removeClass('past');
-      $(timeBlocks[i]).addClass('present');
-    }
-    else{
-      $(timeBlocks[i]).removeClass('past');
-      $(timeBlocks[i]).removeClass('present');
-      $(timeBlocks[i]).addClass('future');
+      //create loop comparing idTime and hour
+      if (idTime < hour){
+        $(timeBlocks[i]).removeClass('present');
+        $(timeBlocks[i]).removeClass('future');
+        $(timeBlocks[i]).addClass('past');
+      }
+      else if (idTime == hour){
+        $(timeBlocks[i]).removeClass('future');
+        $(timeBlocks[i]).removeClass('past');
+        $(timeBlocks[i]).addClass('present');
+      }
+      else{
+        $(timeBlocks[i]).removeClass('past');
+        $(timeBlocks[i]).removeClass('present');
+        $(timeBlocks[i]).addClass('future');
+      }
     }
   }
-}
-//Create a function to update the past, present, future, tags of each time block on a set interval
-function timer() {
-  
-  // TODO: Use the `setInterval()` method to call a function to be executed every 60000 milliseconds (Every minute)
-  var timeInterval = setInterval(function () {
-   setBlocks()
-    // if(timeLeft === 0) {
-    //   // Stops execution of action at set interval
-    //   clearInterval(timeInterval);
-   
-    // }
-  }, 60000);
-}
-
-//setBlocks function defined seperately so it is called immediately and then on an interval after first run
-setBlocks();
-timer();
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 
+  //call setBlocks function so it auto updates every minute
+  function timer() {
+    
+    // function to be executed every 60000 milliseconds (Every minute)
+    var timeInterval = setInterval(function () {
+     setBlocks()
+     
+    }, 60000);
+  }
+  
+  //setBlocks function defined seperately so it is called immediately and then on an interval after first run
+  setBlocks();
+  timer();
+
+
+  //takes any inputs and saves in local data when save button is clicked
   saveButton.on('click',function(){
     console.log(this);
     //need to save text in text area associated with the button clicked to local storage
-    var task = $(this).prev().value;
+    var task = $(this).prev()[0].value;
     
     console.log(task);
     var storageName = $(this).parent().attr('id');
@@ -91,29 +73,21 @@ $(function () {
 
 
    
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+
   
-  
-
-
-
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  //Take data from local storage and print into appropriate text boxes
   for (var i = 0; i < timeBlocks.length; i++) {
     var previousStorage = $(timeBlocks[i]).attr('id');
+   
     previousStorage = localStorage.getItem(previousStorage);
-    $(timeBlocks[i]).children[1].text(previousStorage);
+  
+    var textarea = timeBlocks[i].children[1];
+    //console.log(textarea);
+    $(textarea).text(previousStorage);
   }
 
-  // TODO: Add code to display the current date in the header of the page.
+  //Add date to header of page
    var date = dayjs().format('dddd M/D/YYYY');
   $('#currentDay').text(date);
 });
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Work-Day-Scheduler
-A Scheduling tool that will show you what tasks must be done at certain times.
+
+## Description
+A scheduling tool that allows the user to put tasks into hourly time blocks and save them.  When the webpage is recalled the tasked are still shown in the the time blocks until removed.  The time blocks will be color coded to show wether that hour has already past, is current, or has not yet occured.
+
+## User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Usage
+When the user opens the application hourly timeblocks will appear with any tasks that were previously saved.
+
+![WebsiteScreenshot](./Assets/Screen%20Shot%202023-01-30%20at%2010.10.28%20PM.png)
+
+##
+Deployed Website:
+https://jpbaer.github.io/Work-Day-Scheduler/
